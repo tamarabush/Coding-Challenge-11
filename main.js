8bfca1abe0f5f671e702b439eaf1e753ce10e38b
tamarabush <tamara.abush14@outlook.com>
2024-10-21 20:30:38 -0400

    Set up references to the DOM elements

---
+++ main.js
@@ -0,0 +1,6 @@
+//get references to the DOM elements
+const productSelector = document.getElementById('product-selector');
+const quantityInput = document.getElementById('quantity');
+const totalPriceElement = document.getElementById('total-price');
+const placeOrderButton = document.getElementById('place-order');
+const orderSummary = document.getElementById('order-summary');
