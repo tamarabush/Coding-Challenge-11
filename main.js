ec10bff8e40e1c4194f4136f9b78718cfbdde3ba
tamarabush <tamara.abush14@outlook.com>
2024-10-24 09:51:06 -0400

    Add event listener for product selection and quantity input

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-//get references to the DOM elements
+//Get references to the DOM elements:
+
 const productSelector = document.getElementById('product-selector');
 const quantityInput = document.getElementById('quantity');
 const totalPriceElement = document.getElementById('total-price');
 const placeOrderButton = document.getElementById('place-order');
 const orderSummary = document.getElementById('order-summary');
+
+
+//TASK 1 - Add Event Listener for Product Selection:
+
+productSelector.addEventListener('change', updateTotalPrice);
+quantityInput.addEventListener('change',updateTotalPrice);
